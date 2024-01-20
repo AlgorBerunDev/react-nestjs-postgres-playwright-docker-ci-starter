fb9968daeead587a8df67db3b00513e5fa9c8f60
Doston Xusanov <algorberun@gmail.com>
2024-01-21 01:39:27 +0500

    Removed console.log

--- dashboard/rsbuild.config.ts
+++ dashboard/rsbuild.config.ts
@@ -4,5 +4,5 @@ import { pluginReact } from "@rsbuild/plugin-react";
 const config = defineConfig({
   plugins: [pluginReact()],
 });
-console.log(config);
+
 export default { ...config, dev: { client: { port: 80 } } };
