fee2fc4f8cde1d91a6b828e28096a1524462114a
Doston Xusanov <algorberun@gmail.com>
2024-01-24 17:42:19 +0500

    Configure dashboard/vite.config.ts and docker-compose.yml

--- dashboard/rsbuild.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@rsbuild/core";
-import { pluginReact } from "@rsbuild/plugin-react";
-
-const config = defineConfig({
-  plugins: [pluginReact()],
-});
-
-export default { ...config, dev: { client: { port: 80 } } };
---
+++ dashboard/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    hmr: {
+      port: process.env.WDS_SOCKET_PORT ? parseInt(process.env.WDS_SOCKET_PORT) : 3000,
+    },
+  },
+  clearScreen: false,
+});
