f6453ddcb987cc4095c59a5c01ee8f33edffb0e9
Doston Xusanov <algorberun@gmail.com>
2024-01-29 04:45:15 +0500

    Configured ThemeProvider and react-router-dom with layouts

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,5 +1,32 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AdminLayout from "./layouts/AdminLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import HomePage2 from "./pages/HomePage2";
+
 function App() {
-  return <h1>Head</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AdminLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="2" element={<HomePage2 />} />
+            {/* Определите другие подмаршруты для AdminLayout здесь */}
+          </Route>
+          <Route path="/auth" element={<AuthLayout />}>
+            <Route path="login" element={<LoginPage />} />
+            {/* Определите другие подмаршруты для AuthLayout здесь */}
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ dashboard/src/pages/HomePage2.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage2: React.FC = () => {
+  return <div>Домашняя страница 2</div>;
+};
+
+export default HomePage2;
