ccdbfa2a0b4eeb0b788e3255679db4d1d42c20d7
Doston Xusanov <algorberun@gmail.com>
2024-01-29 00:50:15 +0500

    Configured roles guard

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Roles } from './modules/auth/roles.decorator';
+import Role from './lib/enums/roles.enum';
+import { JwtAuthGuard } from './modules/auth/jwt-auth.guard';
+import { RolesGuard } from './modules/auth/roles.guard';
 
 @Controller()
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Roles(Role.MasterManager)
   users(): any {
     return {};
   }
